356dd22eda033703afa457de7726d432925d17b1
Vikas Jha <vikasjha@students.vnit.ac.in>
2018-10-20 19:34:51 +0530

    Addition implemented

--- main.c
+++ main.c
@@ -1,8 +1,11 @@
 #include<stdio.h>
+#include"mymath.h"
 
 int main()
 {
-	printf("Hello World\n");
+	int a = 10, b = 5;
+	int c = add(a,b);
+	printf("%d\n",c);
 	return 0;
 
 }
---
+++ mymath.h
@@ -0,0 +1,9 @@
+#ifndef __MYMATH__
+#define __MYMATH__
+
+int add(int, int);
+int subtract(int, int);
+int multiply(int, int);
+int divide(int, int);
+
+#endif
